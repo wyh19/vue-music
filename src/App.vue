867c809a557d2f8ef30733395c1eca95e76e83f1
王焱辉 <wyh_19@163.com>
2018-11-13 18:37:19 +0800

    开发首页

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
     <keep-alive>
        <router-view/>
     </keep-alive>
@@ -10,9 +11,11 @@
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 export default {
   components:{
-    MHeader
+    MHeader,
+    Tab,
   }
 }
 </script>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-11
+
     </div>
 </template>
 
